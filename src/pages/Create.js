8df8fedb9326128ea9a462ddac2cd00dbf01ef96
CodingWithDude <codingwithdude@gmail.com>
2022-09-27 14:17:25 -0700

    Added input fields in Create.js
    Added notes, details, note category and submit button
    created data folder with db.json for notes for json server

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import { makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
 
 const useStyles = makeStyles({
-  btn: {
-    backgroundColor: "violet",
-    "&:hover": {
-      backgroundColor: "blue",
-    },
-  },
-  title: {
-    textDecoration: "underline",
+  field: {
+    marginTop: 20,
     marginBottom: 20,
+    display: "block",
   },
 });
 
 export default function Create() {
   const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+  const [category, setCategory] = useState("todos");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    !title ? setTitleError(true) : setTitleError(false);
+    !details ? setDetailsError(true) : setDetailsError(false);
+
+    if (title && details) {
+      console.log(title, details, category);
+    }
+  };
 
   return (
     <Container>
@@ -32,16 +49,55 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
-      <Button
-        className={classes.btn}
-        onClick={() => console.log("clicked")}
-        type="submit"
-        color="secondary"
-        variant="contained"
-        endIcon={<KeyboardArrowRightIcon />}
-      >
-        Submit
-      </Button>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          variant="outlined"
+          label="Note Title"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+        ></TextField>
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          variant="outlined"
+          label="Details"
+          color="secondary"
+          fullWidth
+          multiline
+          minRows={4}
+          required
+          error={detailsError}
+        ></TextField>
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <FormControlLabel value="money" control={<Radio />} label="Money" />
+            <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+            <FormControlLabel
+              value="reminders"
+              control={<Radio />}
+              label="Reminders"
+            />
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+          </RadioGroup>
+        </FormControl>
+        <Button
+          className={classes.btn}
+          type="submit"
+          color="primary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
