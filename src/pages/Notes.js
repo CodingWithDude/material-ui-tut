a2ae207120ac64916b926b276ebe0b6cada37ed4
CodingWithDude <codingwithdude@gmail.com>
2022-09-27 14:44:21 -0700

    Setup mui Grid and Paper for Notes

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { Container } from "@material-ui/core";
 
 export default function Notes() {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8000/notes")
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, []);
+
   return (
-    <div>
-      Notes page
-    </div>
-  )
+    <Container>
+      <Grid container>
+        {notes.map((note) => (
+          <Grid item key={note.id} xs={12} md={6} lg={3}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
