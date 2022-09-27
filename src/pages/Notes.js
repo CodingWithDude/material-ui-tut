e15218f9f8d3516257927a00d3cc49b4363ad4f7
CodingWithDude <codingwithdude@gmail.com>
2022-09-27 15:02:14 -0700

    Created NoteCard component
    NoteCard returns a mui card of with note elements

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import { Container } from "@material-ui/core";
+import NoteCard from "../components/NoteCard";
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -13,10 +13,10 @@ export default function Notes() {
 
   return (
     <Container>
-      <Grid container>
+      <Grid container spacing={3}>
         {notes.map((note) => (
           <Grid item key={note.id} xs={12} md={6} lg={3}>
-            <Paper>{note.title}</Paper>
+            <NoteCard note={note}></NoteCard>
           </Grid>
         ))}
       </Grid>
