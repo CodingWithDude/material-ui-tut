72dd0f5884c95f77670d1e5b65b4842daaffad6d
CodingWithDude <codingwithdude@gmail.com>
2022-09-27 17:15:58 -0700

    Added dynamic Avatars to cards

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,14 +2,39 @@ import React from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
-import { IconButton, makeStyles, Typography } from "@material-ui/core";
+import { Avatar, IconButton, makeStyles, Typography } from "@material-ui/core";
 import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
+import { green, yellow } from "@material-ui/core/colors";
+import { blue, pink } from "@mui/material/colors";
+
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: (note) => {
+      if (note.category === "work") {
+        return yellow[700];
+      }
+      if (note.category === "money") {
+        return green[500];
+      }
+      if (note.category === "todos") {
+        return pink[500];
+      }
+      return blue[500];
+    },
+  },
+});
 
 const NoteCard = ({ note, handleDelete }) => {
+  const classes = useStyles(note);
   return (
     <div>
       <Card elevation={1}>
         <CardHeader
+          avatar={
+            <Avatar className={classes.avatar}>
+              {note.category[0].toUpperCase()}
+            </Avatar>
+          }
           action={
             <IconButton onClick={() => handleDelete(note.id)}>
               <DeleteOutlined />
